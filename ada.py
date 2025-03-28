4bfcda947851cb3d42df946319ba436f01d42192
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-03-28 18:27:23 -0300

    Update ada.py
    v2

--- ada.py
+++ ada.py
@@ -1,7 +1,14 @@
 first_name = "AdA"
-last_name  = "LoVeLAce"
+last_name = "LoVeLAce"
 
-print(first_name.lower(),last_name.lower())
-print(first_name.title(),last_name.title())
-print(first_name.upper(),last_name.upper())
-print("       ",first_name.lower(),last_name.lower())
+# cambian a lower
+print(first_name.lower(), last_name.lower())
+
+# titulo
+print(first_name.capitalize(), last_name.capitalize())
+
+# cambiar a upper
+print(first_name.upper(), last_name.upper())
+
+# lower
+print(first_name.lower(), last_name.lower())
