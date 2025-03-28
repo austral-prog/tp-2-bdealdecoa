a92f7492bdbd04990d8af07aaf5673119375e379
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-03-28 18:33:10 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,14 +1,10 @@
 first_name = "AdA"
 last_name = "LoVeLAce"
 
-# cambian a lower
 print(first_name.lower(), last_name.lower())
 
-# titulo
 print(first_name.capitalize(), last_name.capitalize())
 
-# cambiar a upper
 print(first_name.upper(), last_name.upper())
 
-# lower
-print(first_name.lower(), last_name.lower())
+print("    " + first_name.lower(), last_name.lower())
