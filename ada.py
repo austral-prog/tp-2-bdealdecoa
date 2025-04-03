3c94b13e7a2c1da8d721cd9e09a5c4672ba35b77
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-04-03 14:35:36 -0300

    Update ada.py
    kk

--- ada.py
+++ ada.py
@@ -3,14 +3,10 @@ from colorama import Fore, Style
 first_name = "AdA"
 last_name = "LoVeLAce"
 
-# Convertir a minúsculas (sin color)
 print(first_name.lower(), last_name.lower())
 
-# Convertir a formato título con color naranja (amarillo en terminal)
-print(Fore.YELLOW + first_name.capitalize(), last_name.capitalize() + Style.RESET_ALL)
+print( first_name.capitalize(), last_name.capitalize())
 
-# Convertir a mayúsculas con color azul
-print(Fore.BLUE + first_name.upper(), last_name.upper() + Style.RESET_ALL)
+print( first_name.upper(), last_name.upper() )
 
-# Otra vez en minúsculas (sin color)
 print(first_name.lower(), last_name.lower())
