476a08c594be3158061caf40803aa045353596ea
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-04-03 13:10:30 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,10 +1,14 @@
 first_name = "AdA"
 last_name = "LoVeLAce"
 
+# Convertir a minúsculas
 print(first_name.lower(), last_name.lower())
 
+# Convertir a formato título (capitalización correcta)
 print(first_name.capitalize(), last_name.capitalize())
 
+# Convertir a mayúsculas
 print(first_name.upper(), last_name.upper())
 
-print("    " + first_name.lower(), last_name.lower())
+# Otra vez en minúsculas
+print(first_name.lower(), last_name.lower())
