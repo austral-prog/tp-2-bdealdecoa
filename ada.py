b30cf8dd41229500ae473715f5404da1b66b93fb
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-04-03 15:01:01 -0300

    Update ada.py
    ff

--- ada.py
+++ ada.py
@@ -1,5 +1,5 @@
 first_name = "AdA"
-last_name = "LoVeLAce"
+last_name  = "LoVeLAce"
 space= " "
 
 print(first_name.lower(), last_name.lower())
@@ -9,4 +9,3 @@ print( first_name.capitalize(), last_name.capitalize())
 print( first_name.upper(), last_name.upper() )
 
 print(space*7,first_name.lower(), last_name.lower())
-
