0effe97a93516b7c144c3fd4ac27673577bb0741
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-04-04 11:57:11 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,12 +1,14 @@
-country1 = "Bangladesh"
-country2 = "Barbados"
+def earth():
 
-if country1 < country2:  # Correct way to check which comes first in alphabetical order
-    x_first = True
-    y_first = False
-else:
-    x_first = False
-    y_first = True
+    country1 = "Bangladesh"
+    country2 = "Barbados"
 
-print(f"The result of {country1} comes first in the dictionary than {country2} is {x_first}.")
-print(f"The result of {country2} comes first in the dictionary than {country1} is {y_first}.")
+    if country1 < country2:  # Correct way to check which comes first in alphabetical order
+        x_first = True
+        y_first = False
+    else:
+        x_first = False
+        y_first = True
+
+    print(f"The result of {country1} comes first in the dictionary than {country2} is {x_first}.")
+    print(f"The result of {country2} comes first in the dictionary than {country1} is {y_first}.")
